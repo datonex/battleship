f24e7331aa3b5a63de56d3b4e617c1b77c4df317
Tanya G <cgomwe@ymail.com>
2021-08-24 14:40:48 +0100

    Remove redundant code and comments

--- run.py
+++ run.py
@@ -1,22 +1,21 @@
+# Imports
+
+# Variables
 grid_size = 9
 board = [['.'] * grid_size for i in range(grid_size)]
 
-# def print_board(board_in):
-#     for row in board_in:
-#         if row[0]:
-#             for i in row[0]:
-#                 i = 1
-#                 i += 1
-#                 print(i)
-#             #print(row[0]) 
-#         #print(" ".join(row))
+# Ship Class
+
+# Draw Board Class for computer and for user
+
+# Functions
 
 def print_board(board_in):
-    print("\n  " + " ".join(str(i) for i in range(1, grid_size + 1)))
+    # Use alphabet to represent columns of game board.
+    # https://www.delftstack.com/howto/python/python-alphabet-list/
+    print("\n  " + " ".join(str(i) for i in list(map(chr, range(65, 65 + grid_size)))))
     for i in range(grid_size):
         print(str(i + 1) + " " + " ".join(str(i) for i in board_in[i]))
-    print()
-            
 print_board(board)
 
 
