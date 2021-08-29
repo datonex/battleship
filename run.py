fedc6134ca1afbc8982b7956bf2751d4f08e77e7
Tanya G <cgomwe@ymail.com>
2021-08-29 16:37:22 +0100

    Reformat code and add get_row function

--- run.py
+++ run.py
@@ -17,62 +17,87 @@ total_turns = 2
 
 # Functions
 
+
 def print_board(board_in):
     # Use alphabet to represent columns of game board.
     # https://www.delftstack.com/howto/python/python-alphabet-list/
-    print('\n  ' + ' '.join(str(i) for i in list(map(chr, range(65, 65 + grid_size)))))
+    print('\n  ' + ' '.join(str(i)
+          for i in list(map(chr, range(65, 65 + grid_size)))))
     for i in range(grid_size):
         print(str(i + 1) + ' ' + ' '.join(str(i) for i in board_in[i]))
+
 
 def random_row(board_in):
     row = randint(0, len(board_in) - 1)
     return row
 
+
 def random_col(board_in):
     col = randint(0, len(board_in) - 1)
     return col
 
+
 def letter_and_index_conversion(value, grid_size):
-    number_list = [i for i in range(1,27)]
-    col_dictionary = dict(zip(alphabet_list,number_list))
-    
+    number_list = [i for i in range(1, 27)]
+    col_dictionary = dict(zip(alphabet_list, number_list))
+
     if type(value) == str and value.upper() in col_dictionary:
-        return col_dictionary[value.upper()] # return index
+        return col_dictionary[value.upper()]  # return index
     elif type(value) == int and value > 0 and value <= grid_size:
-        letter = list(col_dictionary.keys())[list(col_dictionary.values()).index(value)]
-        return letter # return Letter
+        letter = list(col_dictionary.keys())[
+            list(col_dictionary.values()).index(value)]
+        return letter  # return Letter
     else:
-        raise ValueError(f'Value entered does not exist in the board.\n Please enter: \n a letter for the column between {alphabet_list[0]} and {alphabet_list[grid_size - 1]} \n a number between 1 and {grid_size} for the row')
+        raise ValueError(
+            f'Value entered does not exist in the board.\n Please enter: \n a letter for the column between {alphabet_list[0]} and {alphabet_list[grid_size - 1]} \n a number between 1 and {grid_size} for the row')
+
+
+def get_row():
+    while True:
+        try:
+            guess = int(
+                input(f'Guess a number between 1 - {grid_size} \n'))
+            if guess in range(1, grid_size + 1):
+                return guess - 1
+            else:
+                print("Bruh! That's not even in the ocean o_O")
+        except ValueError:
+            print(f'\nPlease enter number between 1 and {grid_size}')
+
 
 print_board(board)
 ship_row = random_row(board)
 ship_col = random_col(board) - 1
+# remove after testing
+guess_row = get_row()
+guess_col = get_col()
+
 
 def main():
     for turn in range(total_turns):
-        print(f'Turn: {turn + 1} of {total_turns}')
-        turn += 1
-        
+        print('Turn', turn + 1, 'of', total_turns)
+
         # Remove print statement at end of project
         print(letter_and_index_conversion(ship_col, grid_size), ship_row)
 
-        letter_col = str(input(f'Guess a column between {alphabet_list[0]} and {alphabet_list[grid_size - 1]}: \n')).upper()
-        guess_col = letter_and_index_conversion(letter_col, grid_size)
-        guess_row = int(input(f'Guess a row number between 1 and {grid_size}: \n'))
-        
-        print(f'You entered: {letter_col.upper()}{guess_row}')
-        
+        guess_col = get_col()
+        guess_row = get_row()
+
+        #print(f'You entered: {letter_col.upper()}{guess_row}')
+
         if guess_row == ship_row and guess_col == ship_col:
             board[guess_row - 1][guess_col - 1] = 'X'
             print_board(board)
             print('Congratulations Captain! You got a hit!')
         else:
-            smallest_col_value = letter_and_index_conversion(alphabet_list[0], grid_size)
-            largest_col_value = letter_and_index_conversion(alphabet_list[grid_size - 1], grid_size)
-            
-            if (guess_row < 1 or guess_row > grid_size) or (guess_col < smallest_col_value or guess_col > largest_col_value):
-                print("Bruh! That's not even in the ocean o_O")
-            elif board[guess_row - 1][guess_col - 1] == 'X' or board[guess_row - 1][guess_col - 1] == '*':
+            smallest_col_value = letter_and_index_conversion(
+                alphabet_list[0], grid_size)
+            largest_col_value = letter_and_index_conversion(
+                alphabet_list[grid_size - 1], grid_size)
+
+            # if (guess_row < 1 or guess_row > grid_size) or (guess_col < smallest_col_value or guess_col > largest_col_value):
+            #    print("Bruh! That's not even in the ocean o_O")
+            if board[guess_row - 1][guess_col - 1] == 'X' or board[guess_row - 1][guess_col - 1] == '*':
                 print('You guessed this one already -_-')
             else:
                 print('Your aim is WAY off!')
@@ -82,4 +107,7 @@ def main():
                     turn = 0
                 print_board(board)
 
-main()
+
+# main()
+
+# END
