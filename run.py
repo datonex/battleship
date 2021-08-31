f00fa9f59c69c966947f648f355cb401f5db8ffe
Tanya G <cgomwe@ymail.com>
2021-08-31 10:11:28 +0100

    Fix game over prompt

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 # Imports
-from random import randint
+import random
 import math
 import string
 
@@ -8,11 +8,8 @@ import string
 grid_size = 9
 board = [['.'] * grid_size for i in range(grid_size)]
 alphabet_list = list(string.ascii_uppercase)
-turn = 0
 total_turns = 2
-# total_turns = math.floor(grid_size**2/1.25)
-
-# Ship Class
+# total_turns = math.floor(grid_size**2/1 35)
 
 # Draw Board Class for computer and for user
 
@@ -29,12 +26,12 @@ def print_board(board_in):
 
 
 def random_row(board_in):
-    row = randint(1, len(board_in) - 1)
+    row = random.randint(1, len(board_in) - 1)
     return row
 
 
 def random_col(board_in):
-    col = randint(1, len(board_in) - 1)
+    col = random.randint(1, len(board_in) - 1)
     return col
 
 
@@ -56,7 +53,7 @@ def get_row():
     while True:
         try:
             guess = int(
-                input(f'Guess a row: \n'))
+                input('Guess a row: \n'))
             if guess in range(1, grid_size + 1):
                 return guess
             else:
@@ -69,7 +66,7 @@ def get_col():
     while True:
         try:
             guess_letter = str(input(
-                f'Guess a column: \n')).upper()
+                'Guess a column: \n')).upper()
             guess = letter_and_index_conversion(guess_letter, grid_size)
             if guess in range(1, grid_size + 1):
                 return guess
@@ -86,39 +83,40 @@ ship_col = random_col(board) - 1
 
 
 def main():
-    for turn in range(total_turns):
-        print('Turn', turn + 1, 'of', total_turns)
+    turn = 0
+    print(turn)
+    while turn < total_turns:
+        print('Turn', turn + 1, 'of', total_turns)        
 
         # Remove print statement at end of project
         print(letter_and_index_conversion(ship_col, grid_size), ship_row)
 
         guess_col = get_col()
         guess_row = get_row()
+        turn += 1
 
+        print('-' * 35)
         print(
-            f'You entered: {letter_and_index_conversion(guess_col, grid_size)}{guess_row}')
+            f'You entered: {letter_and_index_conversion(guess_col, grid_size)}{guess_row} \n')
 
         if guess_row == ship_row and guess_col == ship_col:
             board[guess_row][guess_col] = 'X'
-            print_board(board)
             print('Congratulations Captain! You got a hit!')
+            print_board(board)
+            print('-' * 35)
         else:
-            smallest_col_value = letter_and_index_conversion(
-                alphabet_list[0], grid_size)
-            largest_col_value = letter_and_index_conversion(
-                alphabet_list[grid_size - 1], grid_size)
-
-            # if (guess_row < 1 or guess_row > grid_size) or (guess_col < smallest_col_value or guess_col > largest_col_value):
-            #    print("Bruh! That's not even in the ocean o_O")
             if board[guess_row - 1][guess_col - 1] == 'X' or board[guess_row - 1][guess_col - 1] == '*':
                 print('You guessed this one already -_-')
+
             else:
-                print('Your aim is WAY off!')
+                print('Your aim is WAY off! \n')
                 board[guess_row - 1][guess_col - 1] = '*'
+                print_board(board)
+                print('-' * 35)
                 if turn == total_turns:
                     print('Game Over! You ran out of turns')
-                print_board(board)
-
+                    print('-' * 35)
+                    
 
 main()
 
